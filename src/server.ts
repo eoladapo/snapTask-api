764ff2abad0ea4a6007d0b61c08fb2b4a1fe5861
oladapo <oladapoelijah49@gmail.com>
2025-11-20 01:36:15 +0100

    update

--- src/server.ts
+++ src/server.ts
@@ -24,7 +24,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://snap-task-ui.vercel.app");
+  res.header("Access-Control-Allow-Origin", ["https://snap-task-ui.vercel.app", "http://localhost:5173"]);
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-retry-count");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
