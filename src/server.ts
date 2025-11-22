87ab03f11c13ef4a8e49cdcfdf9c735d1be3143d
oladapo <oladapoelijah49@gmail.com>
2025-11-22 22:58:04 +0100

    fix cors issue

--- src/server.ts
+++ src/server.ts
@@ -100,7 +100,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin",  "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin",  "https://snaptask-geo.onrender.com");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-retry-count");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
